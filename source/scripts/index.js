d0e572d82cc16b71223a6dee7be88552ee7a64b1
Vlad Koronchik <81505769+vladislavkoronchik@users.noreply.github.com>
2024-09-21 00:37:58 +0300

    Add adaptive grid (#12)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,73 @@
-/* в этот файл добавляет скрипты*/
+class CompareSlider {
+  constructor(options = {}) {
+    this.$wrapper = document.querySelector(options.sliderSelector || '.compare-slider');
+
+    if (!this.$wrapper) {
+      return 0;
+    }
+
+    this.$range = this.$wrapper.querySelector(options.inputRangeSelector || '.compare-slider__range');
+
+    if (!this.$range) {
+      return 0;
+    }
+
+    this.currentPx = this.rescale(50, this.$range.min, this.$range.max, 0, this.maxVal);
+    this.mediaQuery = window.matchMedia('(min-width: 768px)');
+
+    this.init();
+  }
+
+  changeRangeWidth() {
+    this.setMaxRangeValue();
+    this.slide(this.$range.value);
+  }
+
+  setMaxRangeValue() {
+    this.maxVal = this.$wrapper.getBoundingClientRect().width;
+  }
+
+  rescale(currentValue, oldMin, oldMax, newMin, newMax) {
+    return (currentValue - oldMin) * (newMax - newMin) / (oldMax - oldMin) + newMin;
+  }
+
+  slide(value) {
+    this.currentPx = this.rescale(value, this.$range.min, this.$range.max, 0, this.maxVal);
+    this.$wrapper.style.setProperty('--dynamic-column-width', `${this.$range.max - value}%`);
+    this.$wrapper.style.setProperty('--separator-position', `${this.currentPx - this.maxVal / 2}px`);
+  }
+
+  init() {
+    this.setMaxRangeValue();
+
+    this.$range.addEventListener('input', () => this.slide(this.$range.value));
+
+    this.mediaQuery.addEventListener('change', (event) => this.changeRangeWidth(event.matches));
+  }
+}
+
+new CompareSlider();
+
+function toggleNav() {
+  const navToggle = document.querySelector('.toggle-nav');
+  const nav = document.querySelector('.nav');
+
+  if (!nav) {
+    return;
+  }
+
+  nav.setAttribute('style', 'transition: unset');
+  nav.firstElementChild.setAttribute('style', 'transition: unset');
+  nav.classList.remove('_nojs');
+  setTimeout(() => {
+    nav.removeAttribute('style');
+    nav.firstElementChild.removeAttribute('style');
+  }, 300);
+
+  navToggle?.addEventListener('click', function() {
+    this.classList.toggle('active');
+    nav.classList.toggle('active');
+  });
+}
+
+toggleNav();
